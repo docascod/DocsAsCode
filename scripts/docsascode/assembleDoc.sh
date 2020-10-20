2dc7729e03ec468b21a943cd24def9d8283ba73a
Jerome <jerome.carre@objectif-libre.com>
2020-10-20 22:13:56 +0200

    #55 #56

--- scripts/docsascode/assembleDoc.sh
+++ scripts/docsascode/assembleDoc.sh
@@ -2,7 +2,13 @@
 # set -xe
 
 allpdf=""
+
 outputPDF="out.pdf"
+[[ ! -z "$DEFAULT_NAME" ]] && outputPDF=$DEFAULT_NAME
+
+destination_folder="."
+[[ ! -z "$DEFAULT_DESTINATION" ]] && destination_folder=$DEFAULT_DESTINATION
+
 while [[ $# -gt 0 ]]
 do
 
@@ -16,7 +22,7 @@ do
             shift
             ;;
     *)
-            echo "extension not supported. only pdf."
+            printf "extension not supported. only pdf.\n"
             exit -1
             ;;
     esac
@@ -25,10 +31,17 @@ do
 
 done
 
+
 if [ ! -z "$allpdf" ] 
 then
-    echo "assemble: "$allpdf
-    qpdf --empty $outputPDF --pages $allpdf "--"
+    echo -e "assemble: "$allpdf
+    qpdf --empty $destination_folder/$outputPDF --pages $allpdf "--"
+    if [ -f $destination_folder/$outputPDF ]; then
+      printf "  → file generated: "$destination_folder"/"$outputPDF"\n"
+      generatedFiles=( "$destination_folder/$outputPDF" )
+    else
+      printf "  → NO file generated\n"
+    fi
 else
-    echo "nothing to assemble"
+    printf "nothing to assemble\n"
 fi
