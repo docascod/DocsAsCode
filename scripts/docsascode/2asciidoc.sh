f8026aba30a3bc9a887223af6a1561c6674a1977
Jerome <jerome.carre@objectif-libre.com>
2020-10-27 21:51:03 +0100

    #59 ifdef, ifndef implementation, tested with md & rst

--- scripts/docsascode/2asciidoc.sh
+++ scripts/docsascode/2asciidoc.sh
@@ -43,7 +43,13 @@ case "$1" in
         sed -i -e "s/\.\. container:: newslide/<<</g" $2.tmp
 
         sed -i -e "s/^.. container:: ifeval \(.*\)$/\n\/\/ ifeval \1\n/g" $2.tmp
-        sed -i -e "s/^.. container:: end_ifeval$/\n\/\/ end_ifeval \n/g" $2.tmp
+        sed -i -e "s/^.. container:: end_ifeval$/\n\/\/ end_if \n/g" $2.tmp
+
+        sed -i -e "s/^.. container:: ifdef \(.*\)$/\n\/\/ ifdef \1\n/g" $2.tmp
+        sed -i -e "s/^.. container:: end_ifdef$/\n\/\/ end_if \n/g" $2.tmp
+
+        sed -i -e "s/^.. container:: ifndef \(.*\)$/\n\/\/ ifndef \1\n/g" $2.tmp
+        sed -i -e "s/^.. container:: end_ifndef$/\n\/\/ end_if \n/g" $2.tmp
 
         sed -i -e "s/^\([ \t]*\).. container:: sliderow/\1$twoColsStart/g" $2.tmp
         sed -i -e "s/^\([ \t]*\).. container:: slidecol/\1$twoColsRow/g" $2.tmp
@@ -86,7 +92,14 @@ rm -f $tmpfile
 
 # ifeval support
 sed -i "s/\/\/ ifeval \(.*\)$/ifeval::\[\1\]/g" $2
-sed -i "s/\/\/ end_ifeval/endif::\[\]/g" $2
+
+# ifdef support
+sed -i "s/\/\/ ifdef \(.*\)$/ifdef::\1\[\]/g" $2
+
+# ifndef support
+sed -i "s/\/\/ ifndef \(.*\)$/ifndef::\1\[\]/g" $2
+
+sed -i "s/\/\/ end_if/endif::\[\]/g" $2
 
 # fix image bloc vs inline
 sed -i "s/^image:\([^:].*\)\[\([^]]*\)\]$/image::\1[\2]/g" $2
