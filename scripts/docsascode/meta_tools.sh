0ca4d884c3924e734e44d5c1bcbf63d3ca58e561
Jerome <jerome.carre@objectif-libre.com>
2020-11-02 14:09:54 +0100

    #57 ok

--- scripts/docsascode/meta_tools.sh
+++ scripts/docsascode/meta_tools.sh
@@ -2,6 +2,7 @@
 # set -xe
 
 metaFile=/tmp/meta.yml
+customMetaFile="__NOFILE__"
 
 function initMeta {
   if [ "$#" -ne 1 ]; then
@@ -40,45 +41,49 @@ function initMeta {
     exit -1
     ;;
   esac
+
+  customMetaFile=$(readInMeta extra_meta $customMetaFile)
+  if [ ! $customMetaFile = '__NOFILE__' ] && [ ! -f $currentdir/$customMetaFile ]; then
+    printf "extra metadata file "$customMetaFile" not exists -> ignored\n"
+    customMetaFile="__NOFILE__"
+  fi
 }
 
 function readInMeta {
   local varPath=$1
   local defaultVal=""
+  local result="__NOTHING__"
 
   ## if have a second param (default value)
-  if [[ "$#" == 2 ]]; then
+  if [ "$#" == 2 ]; then
     defaultVal=$2
   fi
 
-  local result=""
-  ## type of param (single or list)
-  local type=$(yq r --defaultValue __NOTHING__ $metaFile $varPath.t)
-  ## if type not found => default value
-  if [ "$type" = '__NOTHING__' ]
-  then
+  [ $customMetaFile != '__NOFILE__' ] && result=$(yq r --defaultValue __NOTHING__ $customMetaFile $varPath)
+
+  if [ "$result" = '__NOTHING__' ]; then
+
+    ## type of param (single or list)
+    local type=$(yq r --defaultValue __NOTYPE__ $metaFile $varPath.t)
+
+    case "$type" in
+      '__NOTYPE__' )
+        result="__NOTHING__"
+        ;;
+      'MetaInlines' )
+        result=$(yq r --defaultValue __NOTHING__ $metaFile $varPath.c.*.c)
+        ;;
+      'MetaList' | 'MetaBlocks')
+        result=$(yq r --defaultValue __NOTHING__ $metaFile $varPath.c.*.c.*.c)
+        ;;
+    esac
+
+  fi
+
+  if [ "$result" = '__NOTHING__' ]; then 
     echo $defaultVal
-    ## if type is single
-  elif [ "$type" = 'MetaInlines' ]
-  then
-    ## get value
-    result=$(yq r --defaultValue __NOTHING__ $metaFile $varPath.c.*.c)
-    ## if nothing in result 
-    if [ "$result" = '__NOTHING__' ]
-    then
-      echo $defaultVal
-    else
-      echo $result
-    fi
-  elif [ "$type" = 'MetaList' ] || [ "$type" = 'MetaBlocks' ]
-  then 
-    result=$(yq r --defaultValue __NOTHING__ $metaFile $varPath.c.*.c.*.c)
-    if [ "$result" = '__NOTHING__' ]
-    then
-      echo $defaultVal
-    else
-      echo $result      
-    fi
+  else
+    echo $result      
   fi
 }
 
@@ -93,6 +98,13 @@ function getExtraMetaKeys {
   do 
     [[ ! " ${classicalKeys[*]} " == *"$elem"* ]] && extraKeys+=( "$elem" ) 
   done
+
+  [ $customMetaFile != '__NOFILE__' ] && allKeysExtra=( $(yq r --printMode p $customMetaFile '*') )
+  for elem in "${allKeysExtra[@]}"
+  do 
+    [[ ! " ${classicalKeys[*]} " == *"$elem"* ]] && extraKeys+=( "$elem" ) 
+  done
+
   echo ${extraKeys[@]}
 }
 
